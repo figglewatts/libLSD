c71fbf5898dcea6d9f89af5d4ab3d32c16a722f2
Figglewatts <figglewatts@hotmail.co.uk>
2017-11-07 19:13:44 +0000

    Added FixedPoint and Vec3 types

---
+++ libLSD/Types/FixedPoint.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libLSD.Types
+{
+    /// <summary>
+    /// A 16-bit fixed point number as used in PSX hardware.
+    /// 
+    /// In the form:
+    /// <code>
+    ///     MSB ----------- LSB
+    ///     SIII MMMM MMMM MMMM
+    /// </code>
+    /// Where S is sign bit, I are integral bits, and M are mantissa bits
+    /// </summary>
+    public class FixedPoint
+    {
+        public int IntegralPart
+        {
+            get => (this._value & INTEGRAL_MASK) >> 12;
+            set => this._value = _value ^ ((_value ^ (value << 12)) & INTEGRAL_MASK);
+        }
+
+        public int DecimalPart
+        {
+            get => (this._value & MANTISSA_MASK);
+            set => this._value = _value ^ ((_value ^ value) & MANTISSA_MASK);
+        }
+
+        public bool IsNegative
+        {
+            get => (this._value & SIGN_MASK) >> 15 == 1;
+            set => this._value = _value ^ ((_value ^ ((value ? 1 : 0) << 15)) & SIGN_MASK);
+        }
+
+        private int _value;
+
+        const int SIGN_MASK = 0x8000;
+        const int INTEGRAL_MASK = 0x7000;
+        const int MANTISSA_MASK = 0xFFF;
+
+        public FixedPoint()
+        {
+            _value = 0;
+        }
+
+        public FixedPoint(byte[] data)
+        {
+            if (data.Length != 2) { throw new ArgumentException("data must be 2 bytes", nameof(data)); }
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(data);
+            _value = BitConverter.ToUInt16(data, 0);
+        }
+
+        public override string ToString()
+        {
+            return (IsNegative ? "-" : "") + IntegralPart + "." + DecimalPart;
+        }
+
+        public static implicit operator float(FixedPoint p)
+        {
+            return p._value / 65536.0f;
+        }
+    }
+}
---
+++ libLSD/Types/Vec3.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libLSD.Types
+{
+    public struct Vec3
+    {
+        public float X;
+        public float Y;
+        public float Z;
+
+        public Vec3(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public static Vec3 operator +(Vec3 a, Vec3 b)
+        {
+            Vec3 returnVal = new Vec3
+            {
+                X = (a.X + b.X),
+                Y = (a.Y + b.Y),
+                Z = (a.Z + b.Z)
+            };
+            return returnVal;
+        }
+
+        public static Vec3 operator -(Vec3 a)
+        {
+            Vec3 returnVal = new Vec3
+            {
+                X = -a.X,
+                Y = -a.Y,
+                Z = -a.Z
+            };
+            return returnVal;
+        }
+
+        public static Vec3 operator -(Vec3 a, Vec3 b)
+        {
+            return a + -b;
+        }
+
+        public static Vec3 operator *(Vec3 a, float s)
+        {
+            Vec3 returnVal = new Vec3
+            {
+                X = (a.X * s),
+                Y = (a.Y * s),
+                Z = (a.Z * s)
+            };
+            return returnVal;
+        }
+
+        public static Vec3 operator /(Vec3 a, float s)
+        {
+            Vec3 returnVal = new Vec3
+            {
+                X = (a.X / s),
+                Y = (a.Y / s),
+                Z = (a.Z / s)
+            };
+            return returnVal;
+        }
+
+        public static float Dot(Vec3 a, Vec3 b)
+        {
+            return a.X * b.X + a.Y * b.Y + a.Z * b.Z;
+        }
+
+        public static Vec3 Cross(Vec3 a, Vec3 b)
+        {
+            Vec3 returnVal = new Vec3
+            {
+                X = a.Y * b.Z - a.Z * b.Y,
+                Y = a.Z * b.X - a.X * b.Z,
+                Z = a.X * b.Y - a.Y * b.X
+            };
+            return returnVal;
+        }
+
+        public static float Magnitude(Vec3 a)
+        {
+            return (float)Math.Sqrt(a.X * a.X + a.Y * a.Y + a.Z * a.Z);
+        }
+
+        public static Vec3 Normalize(Vec3 a)
+        {
+            return a / Magnitude(a);
+        }
+    }
+}
